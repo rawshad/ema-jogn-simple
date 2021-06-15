26c9d128e1a1e792192b3ff160a8613533ad1b28
rawshad <rawshad@outlook.com>
2021-06-15 23:23:28 +0600

    product added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import fakeData from '../../fakeData'
+import fakeData from '../../fakeData';
+import Product from '../Products/Product';
 
 const Shop = () => {
-    const first10 = fakeData.slice(0,10);
+    const first10 = fakeData.slice(0, 10);
     const [products, fakeProducts] = useState(first10);
+    console.log(products);
     return (
-        <div>
-            <h1>This is shop</h1>
-            <h3>{products.length}</h3>
-            <ul>
-                {
-                    products.map(product => <li>{product.name}</li>)
-                }
-            </ul>
-        </div>
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-9 border-end border-2">
+                        <ul>
+                            {
+                                products.map(product => <Product product={product} key={product.key}></Product> )
+                            }
+                        </ul>
+                    </div>
+                    <div className="col-md-3">
+                        <h1>This is cart</h1>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
